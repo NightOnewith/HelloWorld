7b59f65bf2e3365136f57f06c7f6b2338f3a0827
yzj <y347951803@outlook.com>
2017-08-12 00:02:47 +0800

    测试

---
+++ src/com/mly/helloworld/Helloworld.java
@@ -0,0 +1,10 @@
+package com.mly.helloworld;
+
+/**
+ * Created by ASUS on 2017/8/11.
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
